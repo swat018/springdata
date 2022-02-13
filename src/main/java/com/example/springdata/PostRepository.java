280c8fc1fe32b9bcd3d34e606502750b4aee7833
jinwoo park <swat018@gmail.com>
2022-02-13 13:22:25 +0900

    feat : 스프링 데이터 Common - Repository

--- src/main/java/com/example/springdata/PostRepository.java
+++ src/main/java/com/example/springdata/PostRepository.java
@@ -1,7 +1,12 @@
 package com.example.springdata;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 }
